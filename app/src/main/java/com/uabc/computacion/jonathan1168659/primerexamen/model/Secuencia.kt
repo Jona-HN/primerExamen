82ab3b7bc039f5f33a0d48b8e25977afa11f059b
Jonathan Herrera <msjona28@outlook.com>
2022-04-27 00:20:29 -0700

    Change: El tipo de dato del atributo elementos (ArrayList -> Array), ya que nunca se añaden más elementos a la secuencia en tiempo de ejecución. Change: el nombre del método getNew -> getNewInstance. Ya que hace más sentido debido a que regresa una instancia (o copia) de una secuencia predefinida. New: más secuencias añadidas a cada nivel de dificultad. New: dos nuevos atributos: elementosVisibles, que representa la secuencia que es visible para el jugador. Las incógnitas están marcadas por "_""; tiene su correspondiente toString(). Y fueSeleccionada, que sirve para evitar secuencias duplicadas dentro de un conjunto de 3 secuencias. También se agregó un método para resetear el valor de este atributo en todas las instancias. New: implementación de Clonable, para poder crear copias de una secuencia y evitar modificar las predefinidas. New: operadores get y set, para modificar el atributo elementos (Array) sin tener que hacerlo público.

--- app/src/main/java/com/uabc/computacion/jonathan1168659/primerexamen/model/Secuencia.kt
+++ app/src/main/java/com/uabc/computacion/jonathan1168659/primerexamen/model/Secuencia.kt
@@ -1,30 +1,85 @@
 package com.uabc.computacion.jonathan1168659.primerexamen.model
 
-class Secuencia private constructor(private val elementos: ArrayList<String>)
+class Secuencia private constructor(private val elementos: Array<String>) : Cloneable
 {
     val longitud = elementos.size
+    private val elementosVisibles = elementos.clone()
+    var fueSeleccionada = false
+        private set
 
-    fun compareTo(s : Secuencia) = this.elementos == s.elementos
+    fun compareTo(s : Secuencia) = this.elementos.contentEquals(s.elementos)
 
     override fun toString() = elementos.joinToString(separator = " ")
 
+    fun elementosVisiblesToString() = elementosVisibles.joinToString(separator = " ")
+
+    operator fun get(indice : Int) = elementosVisibles[indice]
+
+    operator fun set(indice : Int, value : String)
+    {
+        elementosVisibles[indice] = value
+    }
+
+    override fun clone() : Secuencia
+    {
+        return Secuencia(elementos)
+    }
+
     companion object
     {
-        private val coleccion : HashMap<Int, HashMap<Int, Secuencia>> = hashMapOf(
-            // TODO: agregar más secuencias
-            1 to hashMapOf(
-                1 to Secuencia(arrayListOf("a", "b", "c", "d")),
-                2 to Secuencia(arrayListOf("1", "2", "3", "4")),
-                3 to Secuencia(arrayListOf("w", "x", "y", "z")),
-                4 to Secuencia(arrayListOf("6", "7", "8", "9")),
-                5 to Secuencia(arrayListOf("1", "0", "1", "0"))
+        private val coleccion : HashMap<Int, Array<Secuencia>> = hashMapOf(
+            1 to arrayOf(
+                Secuencia(arrayOf("a", "b", "c", "d")),
+                Secuencia(arrayOf("e", "f", "g", "h")),
+                Secuencia(arrayOf("1", "2", "3", "4")),
+                Secuencia(arrayOf("w", "x", "y", "z")),
+                Secuencia(arrayOf("6", "7", "8", "9")),
+                Secuencia(arrayOf("1", "0", "1", "0")),
+                Secuencia(arrayOf("0", "1", "0", "1")),
+                Secuencia(arrayOf("1", "2", "4", "8")),
+                Secuencia(arrayOf("16", "32", "64", "128")),
+                Secuencia(arrayOf("14", "11", "8", "5"))
             ),
-            // TODO: agregar las secuencias para los otros niveles
-            2 to hashMapOf(),
-            3 to hashMapOf()
+            2 to arrayOf(
+                // Escala mayor (tonos)
+                Secuencia(arrayOf("do", "re", "mi", "fa", "sol")),
+                Secuencia(arrayOf("fa", "sol", "la", "si", "do")),
+                // Colores del arcoíris
+                Secuencia(arrayOf("rojo", "naranja", "amarillo", "verde", "azul", "morado")),
+                // Planetas del sistema solar
+                Secuencia(arrayOf("mercurio", "venus", "tierra", "marte")),
+                Secuencia(arrayOf("jupiter", "saturno", "urano", "neptuno")),
+                // +2, +3, +4 ... +n
+                Secuencia(arrayOf("4", "6", "9", "13", "18")),
+                // Menos dos y por tres
+                Secuencia(arrayOf("5", "3", "9", "7", "21", "19")),
+                Secuencia(arrayOf("4", "2", "6", "4", "12", "10")),
+                // Números romanos
+                Secuencia(arrayOf("I", "II", "III", "IV", "V")),
+                Secuencia(arrayOf("VI", "VII", "VIII", "IX", "X"))
+            ),
+            3 to arrayOf(
+                // Nombre de la distribución del teclado
+                Secuencia(arrayOf("q", "w", "e", "r", "t", "y")),
+                // Peso de las posiciones en base binaria
+                Secuencia(arrayOf("1", "2", "4", "8", "16", "32")),
+                // Gases nobles
+                Secuencia(arrayOf("He", "Ne", "Ar", "Kr", "Xe", "Rn", "Og")),
+                // Serie de Fibonacci
+                Secuencia(arrayOf("1", "2", "3", "5", "8", "13", "21", "34")),
+                Secuencia(arrayOf("55", "89", "144", "233", "377", "610", "987")),
+                // Entre dos y menos 2^n
+                Secuencia(arrayOf("96", "48", "44", "22", "18", "9", "3")),
+                Secuencia(arrayOf("108", "54", "52", "26", "22", "11", "5")),
+                // Números primos
+                Secuencia(arrayOf("1", "2", "3", "5", "7", "11")),
+                Secuencia(arrayOf("13", "17", "19", "23", "29", "31")),
+                // Alfabeto griego
+                Secuencia(arrayOf("alfa", "beta", "gamma", "delta", "epsilon"))
+            )
         )
 
-        fun getNew(nivel : Int) : Secuencia
+        fun getNewInstance(nivel : Int) : Secuencia
         {
             if (nivel !in 1..coleccion.size)
             {
@@ -34,8 +89,30 @@ class Secuencia private constructor(private val elementos: ArrayList<String>)
             {
                 val secuenciasDelNivel = coleccion[nivel]
                 val numeroSecuencias = secuenciasDelNivel!!.size
+                var secuencia : Secuencia
 
-                return secuenciasDelNivel[(1..numeroSecuencias).random()] as Secuencia
+                do
+                {
+                    secuencia = secuenciasDelNivel[(0 until numeroSecuencias).random()]
+                } while (secuencia.fueSeleccionada)
+
+                secuencia.fueSeleccionada = true
+
+                // Se crea una copia (instancia nueva) de la secuencia seleccionada
+                secuencia = secuencia.clone()
+
+                return secuencia
+            }
+        }
+
+        fun resetearSeleccionDeSecuencias()
+        {
+            for (coleccionSecuencias in coleccion)
+            {
+                for (coleccionesPorNivel in coleccionSecuencias.value)
+                {
+                    coleccionesPorNivel.fueSeleccionada = false
+                }
             }
         }
     }
