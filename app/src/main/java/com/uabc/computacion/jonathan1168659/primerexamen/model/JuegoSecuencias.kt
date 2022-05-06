75480b97b80a2a96fdff28479c46cdd3cd69b80f
Jonathan Herrera <msjona28@outlook.com>
2022-05-05 23:23:18 -0700

    New: se agregaron banderas para comprobar la validez de cada respuesta. New: se agregaron contadores para el turno y el nivel. New: una constante que indica el número de turnos por nivel. New: el atributo 'secuencias' que almacena las secuencias de cada turno. Fix: faltaba la llamada al método resetearSeleccionDeSecuencias dentro de generarTresSecuencias. Change: se cambió la forma de comprobar si la respuesta del jugador fue correcta: almacenando las respuestas en un ArrayList y comparandolas con las del jugador. New: se agregó un método que contiene la lógica necesaria para avanzar en el juego.

--- app/src/main/java/com/uabc/computacion/jonathan1168659/primerexamen/model/JuegoSecuencias.kt
+++ app/src/main/java/com/uabc/computacion/jonathan1168659/primerexamen/model/JuegoSecuencias.kt
@@ -1,16 +1,25 @@
 package com.uabc.computacion.jonathan1168659.primerexamen.model
 
-import android.os.CountDownTimer
 import kotlin.collections.ArrayList
 import kotlin.math.floor
 
 class JuegoSecuencias
 {
-    // TODO: darle funcionalidad al timer
-    lateinit var timer : CountDownTimer
-        private set
     var nivel = 1
+    var turno = 0
+    var jugadorAvanzoTurno = false
+        private set
+    var jugadorAvanzoNivel = false
+        private set
+    var primeraRespuestaCorrecta = false
+        private set
+    var segundaRespuestaCorrecta = false
+        private set
+    var terceraRespuestaCorrecta = false
+        private set
     val secuenciasPorTurno = 3
+    val turnosPorNivel = 5
+    private lateinit var secuencias : ArrayList<Secuencia>
 
     fun generarTresSecuencias() : ArrayList<Secuencia>
     {
@@ -24,7 +33,9 @@ class JuegoSecuencias
             secuencias.add(secuencia)
         }
 
-        return secuencias
+        Secuencia.resetearSeleccionDeSecuencias()
+        this.secuencias = secuencias
+        return this.secuencias
     }
 
     private fun agregarIncognita(secuencia : Secuencia)
@@ -32,6 +43,7 @@ class JuegoSecuencias
         var indiceIncognita : Int
         var numeroIncognitas = 0
         val maximoNumDeIncog = floor(secuencia.longitud / 2.0)
+        val respuestas = ArrayList<String>()
 
         while (numeroIncognitas < nivel && numeroIncognitas < maximoNumDeIncog)
         {
@@ -40,9 +52,68 @@ class JuegoSecuencias
                 indiceIncognita = (0 until secuencia.longitud).random()
             } while (secuencia[indiceIncognita] == "_")
 
+            respuestas.add(secuencia[indiceIncognita])
             secuencia[indiceIncognita] = "_"
 
             numeroIncognitas++
         }
+
+        secuencia.respuestas = respuestas
+    }
+
+    fun inicializarTiempo() : Long
+    {
+        /*
+         * 15 secuencias en total (3 secuencias y 5 turnos por nivel)
+         * Nivel 1 = 10 segundos por secuencia = 150 segundos en total
+         * Nivel 2 = 8 segundos por secuencia = 120 segundos en total
+         * Nivel 3 = 6 segundos por secuencia = 90 segundos en total
+         */
+
+        return (secuenciasPorTurno * turnosPorNivel) * 10 - ((nivel - 1) * 30).toLong()
+    }
+
+    fun comprobarPrimeraRespuesta(respuestasJugador : List<String>)
+    {
+        primeraRespuestaCorrecta = secuencias[0].respuestas.containsAll(respuestasJugador)
+        avanzarTurno()
+    }
+
+    fun comprobarSegundaRespuesta(respuestasJugador : List<String>)
+    {
+        segundaRespuestaCorrecta = secuencias[1].respuestas.containsAll(respuestasJugador)
+        avanzarTurno()
+    }
+
+    fun comprobarTerceraRespuesta(respuestasJugador : List<String>)
+    {
+        terceraRespuestaCorrecta = secuencias[2].respuestas.containsAll(respuestasJugador)
+        avanzarTurno()
+    }
+
+    fun avanzarTurno()
+    {
+        jugadorAvanzoTurno = primeraRespuestaCorrecta && segundaRespuestaCorrecta && terceraRespuestaCorrecta
+
+        if (jugadorAvanzoTurno)
+        {
+            turno++
+            resetearRespuestas()
+
+            jugadorAvanzoNivel = turno == turnosPorNivel
+
+            if (jugadorAvanzoNivel)
+            {
+                turno = 0
+                nivel++
+            }
+        }
+    }
+
+    fun resetearRespuestas()
+    {
+        primeraRespuestaCorrecta = false
+        segundaRespuestaCorrecta = false
+        terceraRespuestaCorrecta = false
     }
 }
